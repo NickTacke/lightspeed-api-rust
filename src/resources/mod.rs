72d57d8fc2e6ece0646a271384bf6012d35654c9
NickTacke <the.nick.tacke@gmail.com>
2024-06-25 23:18:46 +0200

    Module file for all the resources

---
+++ src/resources/mod.rs
@@ -0,0 +1,9 @@
+mod account;
+mod product;
+mod order;
+mod customer;
+
+pub use account::AccountResource;
+pub use product::ProductResource;
+pub use order::OrderResource;
+pub use customer::CustomerResource;
