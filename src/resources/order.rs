f2d1e2b5915d71e92112b4af29556dd5e6eccb4c
NickTacke <the.nick.tacke@gmail.com>
2024-06-26 13:56:04 +0200

    Added model integration for the Order resource

--- src/resources/order.rs
+++ src/resources/order.rs
@@ -1,4 +1,4 @@
-use crate::LightspeedClient;
+use crate::{LightspeedClient, Order, OrderWrapper, Orders, Count};
 use std::error::Error;
 
 pub struct OrderResource<'a> {
@@ -10,27 +10,37 @@ impl<'a> OrderResource<'a> {
         Self { client }
     }
 
-    pub async fn get_all(&self) -> Result<serde_json::Value, Box<dyn Error>> {
-        self.client.read("orders", None).await
-    }
-
-    pub async fn get_by_id(&self, id: &str) -> Result<serde_json::Value, Box<dyn Error>> {
-        self.client.read(&format!("orders/{}", id), None).await
-    }
-
-    pub async fn get_by_number(&self, number: &str) -> Result<serde_json::Value, Box<dyn Error>> {
-        self.client.read("orders", Some(vec![("number", number)])).await
-    }
-
-    pub async fn list(&self, params: Option<Vec<(&str, &str)>>) -> Result<serde_json::Value, Box<dyn Error>> {
+    pub async fn get_all(&self, params: Option<Vec<(&str, &str)>>) -> Result<Orders, Box<dyn Error>> {
         self.client.read("orders", params).await
     }
 
-    pub async fn update(&self, id: &str, order: &serde_json::Value) -> Result<serde_json::Value, Box<dyn Error>> {
-        self.client.update(&format!("orders/{}", id), order).await
+    pub async fn get_by_id(&self, id: &str) -> Result<Order, Box<dyn Error>> {
+        let order: Option<OrderWrapper> = self.client.read(&format!("orders/{}", id), None).await?;
+        
+        Ok(order.and_then(| data | Some(data.order)).ok_or_else(|| std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "No order found with given id!"
+        ))?)
     }
 
-    pub async fn count(&self, params: Option<Vec<(&str, &str)>>) -> Result<serde_json::Value, Box<dyn Error>> {
+    pub async fn get_by_number(&self, number: &str) -> Result<Order, Box<dyn Error>> {
+        let orders: Option<Vec<Order>> = self.client.read("orders", Some(vec![("number", number)])).await?;
+    
+        Ok(orders
+            .and_then(|vec| vec.into_iter().next())
+            .ok_or_else(|| std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "No order found with given number!"
+            ))?)
+    }
+
+    pub async fn update(&self, id: &str, order: Order) -> Result<Order, Box<dyn Error>> {
+        let wrapper = OrderWrapper{order};
+        let response: OrderWrapper = self.client.update(&format!("orders/{}", id), &wrapper).await?;
+        Ok(response.order)
+    }
+
+    pub async fn count(&self, params: Option<Vec<(&str, &str)>>) -> Result<Count, Box<dyn Error>> {
         self.client.read("orders/count", params).await
     }
 }
