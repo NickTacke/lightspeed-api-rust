563294164ff3119c1a3c4e637fb691923929300f
NickTacke <the.nick.tacke@gmail.com>
2024-06-26 14:48:20 +0200

    Added more product functions + Fixed create/update

--- src/resources/product.rs
+++ src/resources/product.rs
@@ -1,5 +1,4 @@
-use crate::LightspeedClient;
-use crate::models::Product;
+use crate::{LightspeedClient, Product, ProductWrapper, Products, Count};
 use std::error::Error;
 
 pub struct ProductResource<'a> {
@@ -11,19 +10,31 @@ impl<'a> ProductResource<'a> {
         Self { client }
     }
 
-    pub async fn create(&self, product: &Product) -> Result<Product, Box<dyn Error>> {
-        self.client.create("products", product).await
+    pub async fn get_all(&self, params: Option<Vec<(&str, &str)>>) -> Result<Products, Box<dyn Error>> {
+        self.client.read("products", params).await
     }
 
-    pub async fn get(&self, id: &str) -> Result<Product, Box<dyn Error>> {
+    pub async fn get_by_id(&self, id: &str) -> Result<Product, Box<dyn Error>> {
         self.client.read(&format!("products/{}", id), None).await
     }
 
-    pub async fn update(&self, id: &str, product: &Product) -> Result<Product, Box<dyn Error>> {
-        self.client.update(&format!("products/{}", id), product).await
+    pub async fn create(&self, product: Product) -> Result<Product, Box<dyn Error>> {
+        let wrapper = ProductWrapper{product};
+        let response: ProductWrapper = self.client.create("products", &wrapper).await?;
+        Ok(response.product)
+    }
+
+    pub async fn update(&self, id: &str, product: Product) -> Result<Product, Box<dyn Error>> {
+        let wrapper = ProductWrapper{product};
+        let response: ProductWrapper = self.client.update(&format!("products/{}", id), &wrapper).await?;
+        Ok(response.product)
     }
 
     pub async fn delete(&self, id: &str) -> Result<(), Box<dyn Error>> {
         self.client.delete(&format!("products/{}", id)).await
+    }
+
+    pub async fn count(&self, params: Option<Vec<(&str, &str)>>) -> Result<Count, Box<dyn Error>> {
+        self.client.read("products/count", params).await
     }
 }
