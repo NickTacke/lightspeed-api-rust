00c462c0a470871a689f174f1155c5d60947e270
NickTacke <the.nick.tacke@gmail.com>
2024-06-26 12:08:35 +0200

    Made the customer resources use the new model system

--- src/resources/product.rs
+++ src/resources/product.rs
@@ -1,4 +1,5 @@
 use crate::LightspeedClient;
+use crate::models::Product;
 use std::error::Error;
 
 pub struct ProductResource<'a> {
@@ -10,19 +11,19 @@ impl<'a> ProductResource<'a> {
         Self { client }
     }
 
-    pub async fn create(&self, product: &serde_json::Value) -> Result<serde_json::Value, Box<dyn Error>> {
+    pub async fn create(&self, product: &Product) -> Result<Product, Box<dyn Error>> {
         self.client.create("products", product).await
     }
 
-    pub async fn get(&self, id: &str) -> Result<serde_json::Value, Box<dyn Error>> {
+    pub async fn get(&self, id: &str) -> Result<Product, Box<dyn Error>> {
         self.client.read(&format!("products/{}", id), None).await
     }
 
-    pub async fn update(&self, id: &str, product: &serde_json::Value) -> Result<serde_json::Value, Box<dyn Error>> {
+    pub async fn update(&self, id: &str, product: &Product) -> Result<Product, Box<dyn Error>> {
         self.client.update(&format!("products/{}", id), product).await
     }
 
-    pub async fn delete(&self, id: &str) -> Result<serde_json::Value, Box<dyn Error>> {
+    pub async fn delete(&self, id: &str) -> Result<(), Box<dyn Error>> {
         self.client.delete(&format!("products/{}", id)).await
     }
 }
