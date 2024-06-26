c3fe677eb20ccaa97d09712ba9ffd19836bbaee2
NickTacke <the.nick.tacke@gmail.com>
2024-06-26 12:07:57 +0200

    Added models to allow for easier usage

---
+++ src/models.rs
@@ -0,0 +1,57 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Count {
+    pub count: i64
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Account {
+    pub id: i64,
+    pub name: String,
+    pub company: String,
+    pub cluster: String,
+    pub language: String,
+    pub created_at: String,
+    pub updated_at: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Product {
+    pub id: i64,
+    pub title: String,
+    pub description: Option<String>,
+    pub price: f64,
+    pub created_at: String,
+    pub updated_at: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Order {
+    pub id: i64,
+    pub number: String,
+    pub status: String,
+    pub total: f64,
+    pub created_at: String,
+    pub updated_at: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Customers {
+    pub customers: Option<Vec<Customer>>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CustomerWrapper {
+    pub customer: Customer
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Customer {
+    pub id: i64,
+    pub firstname: String,
+    pub lastname: String,
+    pub email: String,
+    pub created_at: String,
+    pub updated_at: String,
+}
